437003e0eae1ca1c913156a9a5d2bde695fd12f0
Arnaud Chapeleur <arnaud.chapeleur@gmail.com>
2022-04-29 16:55:08 +0200

    Header

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import logo from '../img/logo-white.png'
+
+const Header = () => {
+  return (
+    <header>
+        <div id="intro">
+            <img src={logo} id='logo-header' alt='Logo Chausse Pieds Blanc' />
+
+            <h1>Cordonnerie / Pressing à Sneakers / Clés / Multiservices</h1>
+
+            <a className='intro-logo' href="https://www.instagram.com/chausse.pieds/" target="_blank"><i className="fab fa-instagram"></i></a>
+
+            <a className='intro-logo' href="https://www.facebook.com/Chausse-Pieds-106818545156437/" target="_blank"><i class="fab fa-facebook-square"></i></a>
+
+            <a className='intro-logo' href="mailto:contact@chaussepieds.fr"><i class="far fa-envelope"></i></a>
+        </div>
+    </header>
+  )
+}
+
+export default Header
