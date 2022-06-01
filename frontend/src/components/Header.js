67638ab8bb08e316cd1d6ed7d65b65f619b02bde
Arnaud Chapeleur <arnaud.chapeleur@gmail.com>
2022-06-01 18:01:42 +0200

    Responsive design

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,42 +1,45 @@
 import React from 'react'
 import logo from '../img/logo-white.png'
+import { Container } from 'react-bootstrap'
 
 const Header = () => {
   return (
     <header>
-      <div id='intro'>
-        <img src={logo} id='logo-header' alt='Logo Chausse Pieds Blanc' />
+      <Container>
+        <div id='intro'>
+          <img src={logo} id='logo-header' alt='Logo Chausse Pieds Blanc' />
 
-        <h1>Cordonnerie / Pressing à Sneakers / Clés / Multiservices</h1>
+          <h1>Cordonnerie / Pressing à Sneakers / Clés / Multiservices</h1>
 
-        <a
-          className='intro-logo'
-          href='https://www.instagram.com/chausse.pieds/'
-          target='_blank'
-        >
-          <i className='fab fa-instagram'></i>
-        </a>
+          <a
+            className='intro-logo'
+            href='https://www.instagram.com/chausse.pieds/'
+            target='_blank'
+          >
+            <i className='fab fa-instagram'></i>
+          </a>
 
-        <a
-          className='intro-logo'
-          href='https://www.facebook.com/Chausse-Pieds-106818545156437/'
-          target='_blank'
-        >
-          <i className='fab fa-facebook-square'></i>
-        </a>
+          <a
+            className='intro-logo'
+            href='https://www.facebook.com/Chausse-Pieds-106818545156437/'
+            target='_blank'
+          >
+            <i className='fab fa-facebook-square'></i>
+          </a>
 
-        <a className='intro-logo' href='mailto:contact@chaussepieds.fr'>
-          <i className='far fa-envelope'></i>
-        </a>
-      </div>
+          <a className='intro-logo' href='mailto:contact@chaussepieds.fr'>
+            <i className='far fa-envelope'></i>
+          </a>
+        </div>
 
-      <div className='read-more'>
-        <p>En savoir plus...</p>
+        <div className='read-more'>
+          <p>En savoir plus...</p>
 
-        <a href='#services'>
-          <i className='fa fa-chevron-down fa-3x bounce'></i>
-        </a>
-      </div>
+          <a href='#services'>
+            <i className='fa fa-chevron-down fa-3x bounce'></i>
+          </a>
+        </div>
+      </Container>
     </header>
   )
 }
